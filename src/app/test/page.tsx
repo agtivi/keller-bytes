960fd59777ffff31dfc2cfe676c43a42843e0cff
Anthony Keller <keller.1190@buckeyemail.osu.edu>
2025-06-15 08:13:08 -0400

    Initial setup for homepage

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,76 +2,26 @@
 import Image from "next/image";
 
 export default function Home() {
-    const jaggedLines = [...Array(10)].flatMap((_, i) => {
-    const segments = [];
-    let lastx = i*10;
-    let lasty = 0;
-
-    for (let h = 0; h < 50; h++) {
-      let next = Math.max(Math.floor(Math.random()*8),3);
-      const temp = Math.random();
-
-      if (temp < 0.15) {
-        segments.push(
-          <line
-            key={`${i}-${h}`}
-            x1={lastx}
-            y1={lasty}
-            x2={lastx+next}
-            y2={lasty}
-            stroke="white"
-            strokeWidth="1"
-          />
-        );
-        lastx = lastx+next;
-      } else if (temp < 0.3) {
-        segments.push(
-          <line
-            key={`${i}-${h}`}
-            x1={lastx}
-            y1={lasty}
-            x2={lastx-next}
-            y2={lasty}
-            stroke="white"
-            strokeWidth="1"
-          />
-        );
-        lastx = lastx-next;
-      } else {
-        segments.push(
-          <line
-            key={`${i}-${h}`}
-            x1={lastx}
-            y1={lasty}
-            x2={lastx}
-            y2={lasty+next}
-            stroke="white"
-            strokeWidth="1"
-          />
-        );
-        lasty = lasty+next;
-      }
-      next = Math.max(Math.floor(Math.random()*8),3);
-    }
-
-    return segments;
-  });
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen font-[family-name:var(--font-geist-sans)]">
-      {/* <div className="line">
-      </div> */}
-      <div className="w-full h-[140px]">
-        <svg className="w-full h-[500px] pt-10" viewBox="0 0 100 100" preserveAspectRatio="none">
-          {jaggedLines}
-        </svg>
-      </div>
-      <main className="gap-[32px] row-start-2 text-center content-center sm:items-start text-4xl font-bodoni-moda">
+    <div className="grid min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <div className="text-center content-center sm:items-start bg-yellow-500 h-176 text-4xl font-bodoni-moda">
         Welcome to KellerBytes
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="text-center content-center">
+          My name's Anthony Keller and I'm a 2025 B.S. in Computer Science and Engineering graduate from the Ohio State University. 
+        </div>
+        <div className="flex content-center h-200 justify-center bg-green-500">
+          <Image
+            src="/suit_photo.jpg"
+            alt="Casual Headshot"
+            width="400"
+            height="100"
+          />
+        </div>
+      </div>
     </div>
   );
 }
